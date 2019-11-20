bc41315789d2feb008180a7dda43d5844fa215ca
“carlosalanis” <juancarlos.alanis@hotmail.comssh-keygen -t rsa -C juancarlos.alanis@hotmail.com>
2019-11-20 16:00:48 -0600

    Initial Commit

---
+++ distro/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  distro
+//
+//  Created by Carlos Alanis on 20/11/19.
+//  Copyright © 2019 FunRobot. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ distro/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  distro
+//
+//  Created by Carlos Alanis on 20/11/19.
+//  Copyright © 2019 FunRobot. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
